1da16a1a937dfdc74a678bb85149d15e8a601c70
Filippo Patrignani <filippo.patrignani2@studio.unibo.it>
2025-03-04 10:41:21 +0100

    Update listcomprehension.py

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,15 +1,9 @@
 l = [x**2 for x in range(1,11)]
 print(l)
 
+l1 = ['ciao', 'daniel', 'cielo', 'albero']
+l2 = [x for x in l1 if x[0] == 'a']
+print(l2)
+
 dizionario = {x:x**2 for x in range(1,6)}
 print(dizionario)
-
-n = -1
-def numSign(n):
-    if n > 0:
-        print('Il numero è positivo.')
-    elif n < 0:
-        print('Il numero è negativo.')
-    else:
-        print('il numero è zero.')
-numSign(n)
