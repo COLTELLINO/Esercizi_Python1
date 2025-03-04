7c94109463f0dee19a72369a43044b20109330c9
Filippo Patrignani <filippo.patrignani2@studio.unibo.it>
2025-03-04 10:56:48 +0100

    Update stringhe.py

--- stringhe.py
+++ stringhe.py
@@ -8,3 +8,20 @@ def contaVocali(s):
             counter += 1
     return counter
 print('Numero Vocali: ', contaVocali(s))
+
+def isPalindrome(s):
+    if s == ''.join(reversed(s)):
+        print('è un palindromo')
+    else:
+        print('non è un palindromo')
+isPalindrome('ciaoaic')
+
+stringa = input('Inserire stringa: ')
+caps = int(input('digita 1 per convertire in maiuscolo, 2 per convertire in minuscolo'))
+
+if caps == 1:
+    print(stringa.upper())
+elif caps == 2:
+    print(stringa.lower())
+else:
+    print('Stringa o valore caps non valido!')
